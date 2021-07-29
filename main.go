3b2d47e492a93c11d900bd18f324de2b42103f6d
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2021-07-30 00:57:45 +0100

    Comment DB connection
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 
-	"github.com/SomtochiAma/smartvac-api/models"
+	//"github.com/SomtochiAma/smartvac-api/models"
 	"github.com/SomtochiAma/smartvac-api/routes"
 )
 
@@ -16,17 +16,17 @@ func main() {
 		log.Fatalf("unable to load .env file: %s", err)
 	}
 
-	db, err := models.Init()
-	if err != nil {
-		log.Fatalf("unable to initialize database: %s", err)
-	}
-
-	postgresDB, err := db.DB()
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer postgresDB.Close()
-	log.Println("Successfully connected to the database.")
+	//db, err := models.Init()
+	//if err != nil {
+	//	log.Fatalf("unable to initialize database: %s", err)
+	//}
+	//
+	//postgresDB, err := db.DB()
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//defer postgresDB.Close()
+	//log.Println("Successfully connected to the database.")
 
 	r := routes.Init()
 
