5ed1a31cb18c6d83b95e260b345b29840ac0234d
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2021-07-30 01:18:18 +0100

    Uncomment database setup
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>

--- main.go
+++ main.go
@@ -1,32 +1,33 @@
 package main
 
 import (
-	//"fmt"
+	"github.com/SomtochiAma/smartvac-api/models"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 
-	//"github.com/SomtochiAma/smartvac-api/models"
+	"github.com/SomtochiAma/smartvac-api/models"
 	"github.com/SomtochiAma/smartvac-api/routes"
 )
 
 func main() {
 	if err := godotenv.Load(); err != nil {
-		log.Fatalf("unable to load .env file: %s", err)
+		log.Printf("unable to load .env file: %s", err)
 	}
 
-	//db, err := models.Init()
-	//if err != nil {
-	//	log.Fatalf("unable to initialize database: %s", err)
-	//}
-	//
-	//postgresDB, err := db.DB()
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//defer postgresDB.Close()
-	//log.Println("Successfully connected to the database.")
+	db, err := models.Init()
+	if err != nil {
+		log.Fatalf("unable to initialize database: %s", err)
+	}
+
+	postgresDB, err := db.DB()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer postgresDB.Close()
+	log.Println("Successfully connected to the database.")
 
 	r := routes.Init()
 
